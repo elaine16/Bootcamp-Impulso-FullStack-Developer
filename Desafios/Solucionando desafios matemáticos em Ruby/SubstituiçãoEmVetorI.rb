ec42a1833d93f95a8a6d24b16e706b0e7f862834
elaine16 <elaine.adm.redes@gmail.com>
2021-02-13 15:45:59 -0300

    Create SubstituiçãoEmVetorI.rb

---
+++ Desafios/Solucionando desafios matemáticos em Ruby/SubstituiçãoEmVetorI.rb
@@ -0,0 +1,30 @@
+/*
+Substituição em Vetor I
+
+Desafio
+Faça um programa que leia um vetor X[10]. Substitua a seguir, todos os valores nulos e negativos do vetor X por 1. Em seguida mostre o vetor X.
+
+Entrada
+A entrada contém 10 valores inteiros, podendo ser positivos ou negativos.
+
+Saída
+Para cada posição do vetor, escreva "X[i] = x", onde i é a posição do vetor e x é o valor armazenado naquela posição.
+
+ 
+Exemplo de Entrada:
+0
+-5
+63
+0
+...
+
+Exemplo de Saída:
+X[0] = 1
+X[1] = 1
+X[2] = 63
+X[3] = 1
+...
+
+*/
+
+dd
