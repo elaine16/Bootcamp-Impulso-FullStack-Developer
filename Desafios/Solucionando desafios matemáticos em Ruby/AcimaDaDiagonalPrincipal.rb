b09d1d4c3b820c619194779263ec94b152627026
elaine16 <elaine.adm.redes@gmail.com>
2021-02-13 15:48:51 -0300

    Create AcimaDaDiagonalPrincipal.rb

---
+++ Desafios/Solucionando desafios matemáticos em Ruby/AcimaDaDiagonalPrincipal.rb
@@ -0,0 +1,31 @@
+
+/*
+Acima da Diagonal Principal
+
+Leia um caractere maiúsculo, que indica uma operação que deve ser realizada e uma matriz M[12][12]. 
+Em seguida, calcule e mostre a soma ou a média considerando somente aqueles elementos que estão acima da diagonal principal da matriz,
+conforme ilustrado abaixo (área verde).
+
+ 
+
+
+Entrada
+A primeira linha de entrada contem um único caractere Maiúsculo O ('S' ou 'M'), indicando a operação (Soma ou Média) que deverá ser realizada com os elementos da matriz. Seguem os 144 valores de ponto flutuante que compõem a matriz.
+
+Saída
+Imprima o resultado solicitado (a soma ou média), com 1 casa após o ponto decimal.
+
+ 
+Exemplo de Entrada	
+S
+1.0
+0.0
+-3.5
+2.5
+4.1
+...
+
+Exemplo de Saída
+12.6
+
+*/
